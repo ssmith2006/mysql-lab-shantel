9f95e9ace6efd4fea62e7141c64f165dbc96eb52
ssmith2006 <shantel.smith@hotmail.com>
2025-08-28 20:58:31 +0000

    created a file containing a pool and CRUD for the sales database table

---
+++ controllers/salesController.js
@@ -0,0 +1,39 @@
+import pool from "../db/cn.js"
+
+export const getSales = async (req, res) => {
+const sql = `SELECT * FROM sales`;
+const sales = await pool.query(sql);
+
+res.json(sales)
+
+}
+
+export const createSales = async (req, res) => {
+const {customer_id, date} = req.body;
+const sql = `INSERT INTO sales (customer_id, date)VALUES(${customer_id}, '${date}')`;
+const results = await pool.query(sql);
+
+res.json({message: "Sale sucessfully recorded"})
+
+}
+
+export const editSales = async (req, res) => {
+const id = req.params.id
+const {customer_id, date} = req.body
+const sql = `UPDATE sales 
+	     SET customer_id =${customer_id}, date = ${date}
+ 	     WHERE sales_id = ${id}`
+const results = await pool.query(sql);
+
+res.json({message: "Sale sucessfully updated"})
+
+}
+
+export const deleteSales = async (req, res) => {
+const id = req.params.id
+const sql = `DELETE FROM sales 
+ 	     WHERE sales_id = ${id}`
+const results = await pool.query(sql);
+
+res.json({message: "Sale sucessfully deleted"})
+}
