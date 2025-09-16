a2b1d4a46e7f8fc839482aa9a5ae1d7bf9d5b96a
ssmith2006 <shantel.smith@hotmail.com>
2025-09-16 01:44:13 +0000

    corrected prefix

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,17 +1,28 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-export const verifyToken = (req, res, next)=>{
-    const auth = req.headers.authorization
-    const [prefix, token] =auth.split ("")
+export const verifyToken = (req, res, next) => {
+  console.log("Authorization header:", req.headers.authorization);
+  
+  const auth = req.headers.authorization;
 
-if(!prefix === 'Bear' || ! token ){
-    return res.status(401).json ("Error: Missing or invalid Authorization Bear Token.")
-}
-try {
-    const payload = jwt.verify(token, process.env.SECRET_KEY)
-    next ()
-} catch {
-    return res.status(401).json("Token is invalid or expired")
-}
+  if (!auth) {
+    return res.status(401).json("Error: Authorization header missing.");
+  }
+
+  const [prefix, token] = auth.split(" ");
+
+  if (prefix !== "Bearer" || !token) {
+    return res
+      .status(401)
+      .json("Error: Missing or invalid Authorization Bearer token.");
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.SECRET_ENV);
     
-}
+    next();
+  } catch (error) {
+    return res.status(401).json("Token is invalid or expired");
+  }
+};
+
