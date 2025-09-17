0c4bf2079cea4341f553ab1ad1ccc81f9d60082a
ssmith2006 <shantel.smith@hotmail.com>
2025-09-17 03:18:25 +0000

    moved the function

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,14 +3,16 @@ import multer from 'multer';
 import {uploadProfilePicture, getUsers, getUserInformation} from "/workspaces/mysql-lab-shantel/controllers/userControllers.js"
 import { verifyToken } from '../middlewares/verifyToken.js';
 
+export const userRouter = express.Router()
+
 const upload = multer({
     storage: multer.memoryStorage(),
     limits: {fileSize:5*1024*1024}
 
 })
 
-export const userRouter = express.Router()
 
-userRouter.get('/users',verifyToken, getUsers)
+
+userRouter.get('/users', getUsers)
 userRouter.get('/users/:id',verifyToken, getUserInformation)
 userRouter.post('/users/:id/upload-picture', verifyToken, upload.single("image"), uploadProfilePicture)
