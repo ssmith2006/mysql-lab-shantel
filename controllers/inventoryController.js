6b02f93a0eb064c7ebc901a77e9ccd273163498f
ssmith2006 <shantel.smith@hotmail.com>
2025-09-17 23:19:50 +0000

    added function for inventory images

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,5 +1,8 @@
 
 import {pool} from "../db/cn.js";
+import { s3 } from "/workspaces/mysql-lab-shantel/utils/s3client.js";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import path from "path";
 
 export const getInventory = async (req, res) => {
   const sql = `SELECT * FROM inventory`;
@@ -85,3 +88,43 @@ export const getLowStockProducts = async (req, res) => {
   const result = await pool.query(sql)
   res.json(result)
 }
+
+export const uploadInventoryImage = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ error: 'Please send an image file in form-data with key "image".' });
+    }
+
+    if (!file.mimetype.startsWith("image/")) {
+      return res.status(400).json({ error: "Only image files are allowed." });
+    }
+
+    const bucket = process.env.S3_BUCKET_NAME;
+    const region = process.env.AWS_REGION;
+
+    const ext = path.extname(file.originalname) ||"";
+    const key = `inventory-picture/${id}-${Date.now()}${ext}`;
+
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: bucket,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      })
+    );
+
+    const url = `https://${bucket}.s3.${region}.amazonaws.com/${key}`;
+
+    const sql = `UPDATE inventory SET inventory_picture_url = '${url}' WHERE product_id = ${id}`;
+    await pool.query(sql);
+
+    return res.json({ message: "Inventory image uploaded!", url });
+  } catch (err) {
+    console.error("Error uploading inventory image:", err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
