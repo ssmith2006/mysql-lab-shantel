6bccd2f8afda8c1ce3a6e1608b5d6764230c1a70
ssmith2006 <shantel.smith@hotmail.com>
2025-09-17 23:20:21 +0000

    added additional routes for inventory images

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import multer from "multer";
+import { uploadInventoryImage } from "/workspaces/mysql-lab-shantel/controllers/inventoryController.js";
 import {
   getInventory,
   createInventory,
@@ -8,16 +9,31 @@ import {
   getInventoryValue,
   getProductSalesHistory,
   getTop5ByUnit,
-  getLowStockProducts
-} from "../controllers/inventoryController.js"
+  getLowStockProducts,
+} from "../controllers/inventoryController.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 export const inventoryRouter = express.Router();
+
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize:5*1024*1024 },
+});
+
+
 
 inventoryRouter.get("/inventory", getInventory);
 inventoryRouter.post("/inventory", createInventory);
 inventoryRouter.put("/inventory", editInventory);
-inventoryRouter.delete("/inventory", deleteInventory)
-inventoryRouter.get('/inventory/value', getInventoryValue);
-inventoryRouter.get('/inventory/:id/history', getProductSalesHistory)//remember to import
-inventoryRouter.get('/inventory/top/units', getTop5ByUnit)
-inventoryRouter.get('/inventory/low-stock/:threshold', getLowStockProducts)
+inventoryRouter.delete("/inventory", deleteInventory);
+inventoryRouter.get("/inventory/value", getInventoryValue);
+inventoryRouter.get("/inventory/:id/history", getProductSalesHistory); //remember to import
+inventoryRouter.get("/inventory/top/units", getTop5ByUnit);
+inventoryRouter.get("/inventory/low-stock/:threshold", getLowStockProducts);
+inventoryRouter.post(
+  "/inventory/:id/upload-image",
+  verifyToken,
+  upload.single("image"),
+  uploadInventoryImage
+);
