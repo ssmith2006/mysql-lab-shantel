fd869ec33c5da24432be5dc9a0a6c7135e8aa02f
ssmith2006 <shantel.smith@hotmail.com>
2025-09-26 00:13:39 +0000

    modified cors

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
 
 import { customerRouter } from "../mysql-lab-shantel/routes/customerRoutes.js";
 import { salesRouter } from "../mysql-lab-shantel/routes/salesRoutes.js";
 import { inventoryRouter } from "../mysql-lab-shantel/routes/inventoryRoutes.js";
 import { sales_InventoryRouter } from "../mysql-lab-shantel/routes/salesInventoryRoutes.js";
-import { logger } from '../mysql-lab-shantel/middlewares/logger.js'
-import { verifyToken } from '../mysql-lab-shantel/middlewares/verifyToken.js';
+import { logger } from "../mysql-lab-shantel/middlewares/logger.js";
+import { verifyToken } from "../mysql-lab-shantel/middlewares/verifyToken.js";
 import { authRouter } from "./routes/authRouter.js";
-import { userRouter} from "../mysql-lab-shantel/routes/userRouter.js";
-import cors from "cors"
+import { userRouter } from "../mysql-lab-shantel/routes/userRouter.js";
+import cors from "cors";
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use((req, res, next) => {
+  console.log("🔍 Origin:", req.headers.origin);
+  console.log("🔍 Method:", req.method);
+  next();
+});
+app.use(
+  cors({
+    origin: [/.githubpreview.dev$/],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 app.use(express.json());
 
 app.use(logger);
 
-app.use(authRouter)
+app.use(authRouter);
 
-
-app.use(verifyToken)
+app.use(verifyToken);
 app.use(customerRouter);
 app.use(salesRouter);
 app.use(inventoryRouter);
 app.use(sales_InventoryRouter);
-app.use(userRouter)
+app.use(userRouter);
 
 app.listen(3000);
