adc19e5e101e7c3134e65fb6234aafdf31b330dc
ssmith2006 <shantel.smith@hotmail.com>
2025-09-28 00:29:30 +0000

    updated customer routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import multer from "multer";
+import { uploadCustomerImage } from "../controllers/customerController.js";
 import {
   getCustomer,
   createCustomer,
@@ -8,9 +9,15 @@ import {
   getCustomerTotalSpent,
   searchCustomersByName,
   getTop5ByRevenue
-} from "../controllers/customerController.js"
+} from "../controllers/customerController.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 export const customerRouter = express.Router();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize:5*1024*1024},
+});
 
 customerRouter.get("/customer", getCustomer);
 customerRouter.post("/customer", createCustomer);
@@ -21,4 +28,10 @@ customerRouter.delete("/customer", deleteCustomer);
 customerRouter.get('/customer/:id/total-spent', getCustomerTotalSpent) //import at the top
 customerRouter.get('/customer/search/:name', searchCustomersByName),//import at the top
 
-customerRouter.get('/customer/top/revenue', getTop5ByRevenue)
+customerRouter.get('/customer/top/revenue', getTop5ByRevenue),
+customerRouter.post(
+  "/inventory/:id/upload-image",
+  verifyToken,
+  upload.single("image"),
+  uploadCustomerImage
+);
