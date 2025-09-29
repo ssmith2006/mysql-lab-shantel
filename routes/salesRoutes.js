2acc799e87d663cee5e4a13257e2ba07c5bd68cc
ssmith2006 <shantel.smith@hotmail.com>
2025-09-29 19:32:46 +0000

    imported function

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -4,13 +4,15 @@ import {
  getSales,
  createSales,
  editSales,
- deleteSales
+ deleteSales,
+ getCustomerSales
 } from "/../workspaces/mysql-lab-shantel/controllers/salesController.js";
 import { addSalesItems } from "/../workspaces/mysql-lab-shantel/controllers/salesInventoryController.js"
 
 export const salesRouter = express.Router();
 
 salesRouter.get("/sales", getSales);
+salesRouter.get("/sales/:customer_id", getCustomerSales)
 salesRouter.post("/sales", createSales);
 salesRouter.post("/sales/:sale_id/items", addSalesItems);
 salesRouter.put("/sales/:id", editSales);
