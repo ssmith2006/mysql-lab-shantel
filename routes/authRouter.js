dadb8857fcf1551d2270adb844e8877ca5c8c665
ssmith2006 <shantel.smith@hotmail.com>
2025-09-29 19:32:16 +0000

    updated register auth

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,26 +1,22 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import { basicAuth, register } from '../controllers/authController.js'
+import express from "express";
+import jwt from "jsonwebtoken";
+import { basicAuth, register } from "../controllers/authController.js";
 
+export const authRouter = express.Router();
 
-export const authRouter = express.Router()
+authRouter.post("/auth/login", basicAuth, (req, res) => {
+  //retrieve info from headers, we want to use dynamic users, we're going to extract the username and not save the password.  We also get to retrieve the 1st item.
+  const credentials = req.headers.authorization.split(" ")[1];
+  const decode = Buffer.from(credentials, `base64`).toString(`utf-8`);
+  const [username] = decode.split(":");
 
-authRouter.post('/auth/login', basicAuth, (req, res) => {
+  const userData = { username };
+  const token = jwt.sign(userData, process.env.SECRET_ENV, { expiresIn: "8h" });
 
-   //retrieve info from headers, we want to use dynamic users, we're going to extract the username and not save the password.  We also get to retrieve the 1st item.
-   const credentials = req.headers.authorization.split(" ")[1];
-   const decode = Buffer.from(credentials, `base64`).toString(`utf-8`);
-   const [username] = decode.split(":");
-
-   const userData = { username }; 
-   const token = jwt.sign(userData, process.env.SECRET_ENV, {expiresIn: '8h'})
-
-   res.status(200).json({
+  res.status(200).json({
     message: "Successfully logged in.",
     token,
-      user: userData
-   })
-})
-authRouter.post('/auth/register', register) 
-  
-})
+    user: userData,
+  });
+});
+authRouter.post("/auth/register", register) 
